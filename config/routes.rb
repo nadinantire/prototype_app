aa3a8e83b237d23ecf7488805e51389b8c1ba107
Hirosaki Ryo <rih60909@gmail.com>
2021-06-03 16:45:14 +0900

    set routes for orders #2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'orders/index'
-  get 'orders/show'
-  get 'orders/new'
-  get 'orders/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :orders
+  resources :accessories
+  resources :items
+  resources :order_overviews
+  resources :attachements
 end
