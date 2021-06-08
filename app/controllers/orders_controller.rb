82ab63f7ec27c491e01223f505b1c53267091fb0
Hirosaki Ryo <rih60909@gmail.com>
2021-06-08 17:15:31 +0900

    set form object for order #2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,57 @@
 class OrdersController < ApplicationController
+  # before_action :set_order, only: [:index, :show, :edit, :update, :destroy]
   def index
+    @orders = Order.includes(:accessories)
+  end
+
+  def new
+    @order = OrderForm.new
+    # @order.build_accessories
+  end
+
+  def create
+    # binding.irb
+    @order = OrderForm.new(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def show
   end
 
-  def new
+  def edit
+    @order = Order.find(params[:id])
+    @order.accessories.build
   end
 
-  def edit
+  def update
+    if @order.update(order_params)
+      redirect_to order_path, notice: "編集しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @order.destroy
+    redirect_to orders_path, notice: "削除しました"
+  end
+
+  def confirm
+    @order = orders.build(order_params)
+    render :new if @order.invalid?
+  end
+
+  private
+  def order_params
+    params.permit(:quantity, :remarks, :delivery, :acceptance_on, :equipment, :name, :numeral, :identification)
   end
 end
+
+  # def set_order
+  #   @order = Order.find(params[:id])
+  # end
+
