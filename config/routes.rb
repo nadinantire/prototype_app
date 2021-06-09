6fa5a9cfc3d68b770dbd5d2df6ed669a33e8a333
Hirosaki Ryo <rih60909@gmail.com>
2021-06-09 14:28:45 +0900

    added guest login function #4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'users#index'
+  post '/users/guest_sign_in', to: 'users#guest_sign_in'
 
   resources :orders
   resources :accessories
