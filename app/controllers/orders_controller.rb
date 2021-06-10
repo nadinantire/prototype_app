5296db2839fb6bc97c732155615688049e7e3a64
Hirosaki Ryo <rih60909@gmail.com>
2021-06-10 17:12:56 +0900

    set CRUD function  for orders #2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,17 @@
 class OrdersController < ApplicationController
   # before_action :set_order, only: [:show, :edit, :update, :destroy]
   def index
-    @orders = Order.includes(:accessories, :order_overviews)
+    @orders = Order.all
   end
 
   def new
-    @order = OrderForm.new
+    @order = Order.new
+    @order.accessories.build
+    @order.order_overviews.build
   end
 
   def create
-    @order = OrderForm.new(order_params)
+    @order = Order.new(order_params)
     if @order.save
       redirect_to root_path
     else
@@ -19,39 +21,33 @@ class OrdersController < ApplicationController
 
   def show
     @orders = Order.where(id: params[:id])
-
   end
 
   def edit
     @order = Order.find(params[:id])
-    @order.accessories.build
   end
 
   def update
+    @order = Order.find(params[:id])
     if @order.update(order_params)
-      redirect_to order_path, notice: "編集しました"
+      redirect_to orders_path, notice: "編集しました"
     else
       render :edit
     end
   end
 
   def destroy
-    @order.destroy
+    Order.find(params[:id]).destroy
     redirect_to orders_path, notice: "削除しました"
   end
 
-  def confirm
-    @order = orders.build(order_params)
-    render :new if @order.invalid?
-  end
+  # def confirm
+  #   @order = orders.build(order_params)
+  #   render :new if @order.invalid?
+  # end
 
   private
   def order_params
-    params.permit(:quantity, :remarks, :delivery, :acceptance_on, :equipment, :parts, :numeral, :identification, :company, :department, :position, :name)
+    params.require(:order).permit(:quantity, :remarks, :delivery, :acceptance_on, accessories_attributes: [:equipment, :parts, :numeral, :identification, :_destroy, :id], order_overviews_attributes: [:company, :department, :position, :name, :_destroy, :id])
   end
 end
-
-  # def set_order
-  #   @order = Order.find(params[:id])
-  # end
-
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,11 @@
 class Order < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
   has_many :accessories, dependent: :destroy
   has_many :items, dependent: :destroy
   has_many :order_overviews, dependent: :destroy
   has_many :attachements, dependent: :destroy
+
+  accepts_nested_attributes_for :accessories, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :order_overviews, reject_if: :all_blank, allow_destroy: true
+  # proc {|attributes| attributes['equipment'].blank? and attributes['parts'].blank? and attributes['numeral'].blank? and attributes['identification'].blank?}, 
 end
